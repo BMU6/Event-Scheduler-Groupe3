e89d05a4af96bb8c10c6db07841d59924976cc73
Bernd Mueller <bernd.mueller5@web.de>
2026-08-17 11:39:26 +0200

    Contains first version of sign in and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./Layout";
+import Home from "./Home";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
 
 export default function App() {
   return (
-    <div> 
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// bernd1@example.com
+// password123
