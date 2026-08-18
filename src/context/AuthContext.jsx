75d7268c8fedf2d7aff6bf4484344310597ab6cf
Mohamed-Saleh-Ali <a_Dr@windowslive.com>
2026-08-18 16:54:38 +0200

    Navbar fixation, Signin Signout method, working-tested

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,29 +1,18 @@
-// src/context/AuthContext.jsx
-import { createContext, useContext, useEffect, useState } from 'react'
-import { decodeToken } from '../utils/api'
+
+import { createContext, useContext, useState } from 'react'
 
 const AuthContext = createContext(null)
 
+
+
 export function AuthProvider({ children }) {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const setLoggedIn = (value) => {
-    setIsLoggedIn(value)
-  }
 
-//   const [token, setToken] = useState(() => localStorage.getItem('e-api-token'))
-//   const user = token ? decodeToken(token) : null
-
-//   useEffect(() => {
-//     if (token) localStorage.setItem('e-api-token', token)
-//     else localStorage.removeItem('e-api-token')
-//   }, [token])
-
-//   const login = (newToken) => setToken(newToken)
-//   const logout = () => setToken(null)
+  const loggedIn = () => setIsLoggedIn(true)
+  const loggedOf = () => setIsLoggedIn(false)
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, loggedIn, loggedOf }}>
       {children}
     </AuthContext.Provider>
   )
@@ -34,3 +23,42 @@ export function useAuth() {
   if (!ctx) throw new Error('useAuth must be used inside <AuthProvider>')
   return ctx
 }
+
+
+// // src/context/AuthContext.jsx
+// import { createContext, useContext, useEffect, useState } from 'react'
+// import { decodeToken } from '../utils/api'
+
+// const AuthContext = createContext(null)
+
+// export function AuthProvider({ children }) {
+
+//   const [isLoggedIn, setIsLoggedIn] = useState(false)
+//   const setLoggedIn = (value) => {
+//     setIsLoggedIn(value)
+//   }
+
+// //   const [token, setToken] = useState(() => localStorage.getItem('e-api-token'))
+// //   const user = token ? decodeToken(token) : null
+
+// //   useEffect(() => {
+// //     if (token) localStorage.setItem('e-api-token', token)
+// //     else localStorage.removeItem('e-api-token')
+// //   }, [token])
+
+// //   const login = (newToken) => setToken(newToken)
+// //   const logout = () => setToken(null)
+
+//   return (
+//     <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }
+
+// export function useAuth() {
+//   const ctx = useContext(AuthContext)
+//   if (!ctx) throw new Error('useAuth must be used inside <AuthProvider>')
+//   return ctx
+// }
+
